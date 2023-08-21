483981670089ff6536cbfb449c3e254a3bc33006
John Spinella <jrspinella@users.noreply.github.com>
2023-08-21 10:23:52 -0400

    Update naming

--- naming.tf
+++ naming.tf
@@ -7,7 +7,7 @@
 data "azurenoopsutils_resource_name" "vm_linux" {
   name          = var.workload_name
   resource_type = "azurerm_linux_virtual_machine"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "vm"])
   use_slug      = var.use_naming
   clean_input   = true
@@ -17,7 +17,7 @@ data "azurenoopsutils_resource_name" "vm_linux" {
 data "azurenoopsutils_resource_name" "vm_windows" {
   name          = var.workload_name
   resource_type = "azurerm_windows_virtual_machine"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.use_naming ? "" : "vm"])
   use_slug      = var.use_naming
   clean_input   = true
@@ -27,7 +27,7 @@ data "azurenoopsutils_resource_name" "vm_windows" {
 data "azurenoopsutils_resource_name" "computer_windows" {
   name          = var.workload_name
   resource_type = "azurerm_windows_virtual_machine"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.use_naming ? "" : "vm"])
   use_slug      = var.use_naming
   clean_input   = true
@@ -37,7 +37,7 @@ data "azurenoopsutils_resource_name" "computer_windows" {
 data "azurenoopsutils_resource_name" "pub_ip" {
   name          = var.workload_name
   resource_type = "azurerm_public_ip"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "pip"])
   use_slug      = var.use_naming
   clean_input   = true
@@ -47,7 +47,7 @@ data "azurenoopsutils_resource_name" "pub_ip" {
 data "azurenoopsutils_resource_name" "nic" {
   name          = var.workload_name
   resource_type = "azurerm_network_interface"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "nic"])
   use_slug      = var.use_naming
   clean_input   = true
@@ -57,7 +57,7 @@ data "azurenoopsutils_resource_name" "nic" {
 data "azurenoopsutils_resource_name" "nsg" {
   name          = var.workload_name
   resource_type = "azurerm_network_security_group"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "nsg"])
   use_slug      = var.use_naming
   clean_input   = true
@@ -67,7 +67,7 @@ data "azurenoopsutils_resource_name" "nsg" {
 data "azurenoopsutils_resource_name" "disk" {
   name          = var.workload_name
   resource_type = "azurerm_managed_disk"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "disk"])
   use_slug      = var.use_naming
   clean_input   = true
@@ -77,7 +77,7 @@ data "azurenoopsutils_resource_name" "disk" {
 data "azurenoopsutils_resource_name" "avset" {
   name          = var.workload_name
   resource_type = "azurerm_availability_set"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "avail"])
   use_slug      = var.use_naming
   clean_input   = true
@@ -87,7 +87,7 @@ data "azurenoopsutils_resource_name" "avset" {
 data "azurenoopsutils_resource_name" "ppg" {
   name          = var.workload_name
   resource_type = "azurerm_proximity_placement_group"
-  prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
+  prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : module.mod_azregions.location_cli]
   suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "proxigrp"])
   use_slug      = var.use_naming
   clean_input   = true
