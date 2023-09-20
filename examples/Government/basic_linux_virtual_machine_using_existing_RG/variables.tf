5e3c59b52e42d1b9869bd7e65e8d07e57da928bb
John Spinella <jrspinella@users.noreply.github.com>
2023-09-20 10:04:25 -0400

    Update folders in examples

---
+++ examples/Government/basic_linux_virtual_machine_using_existing_RG/variables.tf
@@ -0,0 +1,23 @@
+variable "location" {
+  description = "Azure region in which instance will be hosted"
+  type        = string
+  default     = "eastus"
+}
+
+variable "deploy_environment" {
+  description = "Name of the workload's environnement"
+  type        = string
+  default     = "dev"
+}
+
+variable "workload_name" {
+  description = "Name of the workload_name"
+  type        = string
+  default     = "vm-linux"
+}
+
+variable "org_name" {
+  description = "Name of the organization"
+  type        = string
+  default     = "anoa"
+}
